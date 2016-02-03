2eeca29d984d73ce4a0ce905e8dcd97b471b84fa
Dale <drcorns@hotmail.com>
2016-02-03 01:04:17 -0800

    add random background colors and custom app icon

--- app/src/main/java/com/dalecorns/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/dalecorns/funfacts/FunFactsActivity.java
@@ -1,9 +1,11 @@
 package com.dalecorns.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -13,6 +15,8 @@ public class FunFactsActivity extends AppCompatActivity {
     private TextView mFactTextView;
     private Button mShowFactButton;
     private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+    private RelativeLayout mRelativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +24,17 @@ public class FunFactsActivity extends AppCompatActivity {
         //Assign layout views to view variables
         mFactTextView = (TextView) findViewById(R.id.factTextView);
         mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
         View.OnClickListener listener = new View.OnClickListener(){
             @Override
         public void onClick(View v){
-                //randomly select a fact
+                //randomly select a fact and color
                 String fact = mFactBook.getFact();
-                //Update the screen with new fact
+                int color = mColorWheel.getColor();
+                //Update the screen with new fact and color
                 mFactTextView.setText(fact);
+                mRelativeLayout.setBackgroundColor(color);
+                mShowFactButton.setTextColor(color);
             }
         };
         mShowFactButton.setOnClickListener(listener);
